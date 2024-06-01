5a33702ec7a0cc393d6dac2b7c5e2e3f6342ae50
huyhoang843 <huyhoang843@gmail.com>
2024-06-01 22:48:31 +0700

    feat: add 1st post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,10 +6,10 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://huyhoang843.github.io/blog',
-  siteRepo: 'https://github.com/huyhoang843/blog',
+  siteUrl: 'https://huyhoang843.github.io/blog/',
+  // siteRepo: 'https://github.com/huyhoang843',
   // siteLogo: '/static/images/logo.png',
-  // socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/twitter-card.png',
   // mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'huyhoang843@gmail.com',
   github: 'https://github.com/huyhoang843',
@@ -25,13 +25,13 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-      // You may also need to overwrite the script if you're storing data in the US - ex:
-      // src: 'https://us.umami.is/script.js'
-      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-    },
+    // umamiAnalytics: {
+    // We use an env variable for this site to avoid other users cloning our analytics ID
+    // umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // You may also need to overwrite the script if you're storing data in the US - ex:
+    // src: 'https://us.umami.is/script.js'
+    // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+    // },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // },
